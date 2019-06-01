34937b0da1b59a82c5684c2f55a68000f1e3ac8a
Nick Bansal <extreme_wired@msn.com>
2019-06-01 20:14:55 +0100

    blog data added

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema({
+	title: { type: String, required: true },
+	image: {
+		type: String, default: 'https://drawingpenciled.com/library/3197142/top-10-cutest-cat-breeds-these-cats-are-just-generally-beautiful-they-have-long-shiny-fluffy-coats-and-are-just-pretty-cats-plus-they-look-super-cuddly-like-they-might-purr.jpg',
+	},
+	body: { type: String, required: true },
+	created: { type: Date, default: Date.now }
+});
+
+module.exports = model('Blog', blogSchema);
---
+++ seed/seed.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const Blogs = require('../models');
+
+const seedDB = blogsData => {
+	return mongoose.connection
+		.dropDatabase()
+		.then(() => {
+			const blogRaw = Blogs.insertMany(blogsData);
+			return Promise.all([blogRaw]);
+		})
+		.then(blogsDocs => blogsDocs)
+		.catch(console.log);
+};
+
+module.exports = seedDB;
