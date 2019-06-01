9df0d4e9b73783e8cee254e1c509714a592205be
Nick Bansal <extreme_wired@msn.com>
2019-06-01 20:02:39 +0100

    new branch added

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(express.static('public'), bodyParser.json());
 
 const blogSchema = new Schema({
 	title: { type: String, required: true },
-	image: { type: String, required: true },
+	image: {
+		type: String, default: 'https://drawingpenciled.com/library/3197142/top-10-cutest-cat-breeds-these-cats-are-just-generally-beautiful-they-have-long-shiny-fluffy-coats-and-are-just-pretty-cats-plus-they-look-super-cuddly-like-they-might-purr.jpg',
+	},
 	body: { type: String, required: true },
 	created: { type: Date, default: Date.now }
 });
