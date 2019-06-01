cd3fd3e00688c7920d3ed002fc4a4981db45810a
Nick Bansal <extreme_wired@msn.com>
2019-06-01 19:40:42 +0100

    added files to database

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const DB_URL = require('./config');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { Schema, model } = mongoose;
 
 mongoose.connect(DB_URL, { useNewUrlParser: true })
 	.then(console.log(`Database is running on ${DB_URL}`));
@@ -13,4 +14,18 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.static('public'), bodyParser.json());
 
+const blogSchema = new Schema({
+	title: { type: String, required: true },
+	image: { type: String, required: true },
+	body: { type: String, required: true },
+	created: { type: Date, default: Date.now }
+});
+
+const Blog = model('Blog', blogSchema);
+
+Blog.create({
+	title: 'Test blog',
+	image: 'https://drawingpenciled.com/library/3197142/top-10-cutest-cat-breeds-these-cats-are-just-generally-beautiful-they-have-long-shiny-fluffy-coats-and-are-just-pretty-cats-plus-they-look-super-cuddly-like-they-might-purr.jpg',
+	body: 'Test blog body'
+});
 module.exports = app;
