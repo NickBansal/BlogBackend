a01974171f0545573fba1040269f1591ec80733e
Nick Bansal <extreme_wired@msn.com>
2019-06-01 18:50:51 +0100

    config file added

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('app');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const DB_URL = require('./config');
+
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+	.then(console.log(`Database is running on ${DB_URL}`));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 module.export = app;
