21f33518826989fc030879813b713160ac022590
Nick Bansal <extreme_wired@msn.com>
2019-06-30 15:39:28 +0100

    updated dummy blogs

--- seed/testData/index.js
+++ seed/testData/index.js
@@ -1,23 +1,23 @@
-const body = require('./dummyBody');
+const { body1, body2 } = require('./dummyBody');
 module.exports = [
 	{
 		'title': 'Living in the shadow of a great man',
-		'body': body,
+		'body': body1,
 		'label': 'Cats'
 	},
 	{
 		'title': '7 inspirational thought leaders from Manchester UK',
-		'body': body,
+		'body': body2,
 		'label': 'Cats'
 	},
 	{
 		'title': 'They\'re not exactly dogs, are they?',
-		'body': body,
+		'body': body1,
 		'label': 'Dogs'
 	},
 	{
 		'title': 'UNCOVERED: catspiracy to bring down democracy',
-		'body': body,
-		'label': 'Dogs'
+		'body': body2,
+		'label': 'Lions'
 	}
 ];
