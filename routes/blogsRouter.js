87d05c217273dd291ae5859337d059078ed32dc3
Nick Bansal <extreme_wired@msn.com>
2019-07-25 20:33:01 +0100

    deleted the multer middleware

--- routes/blogsRouter.js
+++ routes/blogsRouter.js
@@ -1,33 +1,10 @@
 const blogsRouter = require('express').Router();
 const { sendAllBlogs, sendBlogById, postNewBlog, deleteBlogById, editCurrentBlog } = require('../controllers/blogsCons');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, './uploads/');
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname);
-	},
-});
-
-const fileFilter = (req, file, cb) => {
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-		cb(null, true);
-	}
-	cb(null, false);
-};
-
-const upload = multer({
-	storage,
-	limits: { fileSize: 1024 * 1024 },
-	fileFilter
-});
 
 blogsRouter
 	.route('/')
 	.get(sendAllBlogs)
-	.post(upload.single('productImage'), postNewBlog);
+	.post(postNewBlog);
 
 blogsRouter
 	.route('/:blog_id')
