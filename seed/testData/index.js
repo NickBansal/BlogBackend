680916191807184edef7cf35c2c5f497f9df6a50
Nick Bansal <extreme_wired@msn.com>
2019-07-25 16:33:48 +0100

    base64 function added

--- seed/testData/index.js
+++ seed/testData/index.js
@@ -1,28 +1,32 @@
 const dummy = require('./dummyBody');
-// const image = require('../../public/images/will_haywood_advice_905.jpg');
+const fs = require('fs');
+
+const base64 = (path) => fs.readFileSync(path, { encoding: 'base64' });
+
+
 module.exports = [
 	{
 		'title': 'Living in the shadow of a great man',
 		'body': dummy.body1,
 		'category': 'Cats',
-		'image': 'uploads/will_haywood_advice_905.jpg'
+		'image': base64('uploads/will_haywood_advice_905.jpg')
 	},
 	{
 		'title': '7 inspirational thought leaders from Manchester UK',
 		'body': dummy.body2,
 		'category': 'Cats',
-		'image': 'uploads/will_haywood_advice_905.jpg'
+		'image': base64('uploads/willhaywooddollar_800.jpg')
 	},
 	{
 		'title': 'They\'re not exactly dogs, are they?',
 		'body': dummy.body1,
 		'category': 'Dogs',
-		'image': 'uploads/will_haywood_advice_905.jpg'
+		'image': base64('uploads/willhaywoodhound_700.jpg')
 	},
 	{
 		'title': 'UNCOVERED: catspiracy to bring down democracy',
 		'body': dummy.body2,
 		'category': 'Lions',
-		'image': 'uploads/will_haywood_advice_905.jpg'
+		'image': base64('uploads/willhaywoodshares1_700.jpg')
 	}
 ];
