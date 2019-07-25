f50f7a5d721c70a64e6d96d56fc1ec4b64e386a2
Nick Bansal <extreme_wired@msn.com>
2019-07-25 14:39:24 +0100

    added in multer for file uploads

--- controllers/blogsCons.js
+++ controllers/blogsCons.js
@@ -28,6 +28,7 @@ exports.sendBlogById = (req, res, next) => {
 };
 
 exports.postNewBlog = (req, res, next) => {
+	console.log(req.file);
 	Blogs.create({ ...req.body })
 		.then(blog => {
 			res.send(blog);
--- routes/blogsRouter.js
+++ routes/blogsRouter.js
@@ -1,10 +1,12 @@
 const blogsRouter = require('express').Router();
 const { sendAllBlogs, sendBlogById, postNewBlog, deleteBlogById, editCurrentBlog } = require('../controllers/blogsCons');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 blogsRouter
 	.route('/')
 	.get(sendAllBlogs)
-	.post(postNewBlog);
+	.post(upload.single('productImage'), postNewBlog);
 
 blogsRouter
 	.route('/:blog_id')
